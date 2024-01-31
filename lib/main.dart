523f53ab779090fe4348cc3e3c35c292462ae635
MuhammadYusuf07 <yusuftkr077@gmail.com>
2024-01-31 20:22:52 +0700

    Sudah connect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:login_register_dan_logout/core.dart';
 import 'package:flutter/material.dart';
 import 'package:login_register_dan_logout/state_util.dart';
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp(
+   options: DefaultFirebaseOptions.currentPlatform,
+ );
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
